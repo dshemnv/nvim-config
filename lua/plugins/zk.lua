96bcacdabc73d910790a9f1e2b2e75bc6aa04edb
Denis Shemonaev <denishem.shemonaev@gmail.com>
2025-05-29 17:43:30 +0200

    Updated zk commands to use vim.ui.input instead of vim.fn.input

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -21,12 +21,24 @@ return {
     })
   end,
   keys = {
-    { "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", desc = "Create a new note" },
+    {
+      "<leader>zn",
+      function()
+        vim.ui.input({ prompt = "Title:" }, function(title)
+          require("zk.commands").get("ZkNew")({ title = title })
+        end)
+      end,
+      desc = "Create a new note",
+    },
     { "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", desc = "Open recent notes" },
     { "<leader>zt", "<Cmd>ZkTags<CR>", desc = "Open notes associated with the selected tags" },
     {
       "<leader>zf",
-      "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>",
+      function()
+        vim.ui.input({ prompt = "Search:" }, function(search)
+          require("zk.commands").get("ZkNotes")({ sort = { "modified" }, match = { search } })
+        end)
+      end,
       desc = "Search in notes",
     },
     { "<leader>zf", ":'<,'>ZkMatch<CR>", desc = "Match current selection", mode = "v" },
